a7f78591aaaea2529114d5f6f8f7d5cbb8a081fe
Woojin Kim <woojinwork16@gmail.com>
2025-01-12 20:04:36 +0900

    update DynamicArray/README.md

--- Array/DynamicArray/test.cpp
+++ Array/DynamicArray/test.cpp
@@ -12,29 +12,19 @@ void print_array(const T& array) {
 }
 
 int main() {
-    DynamicArray<int> arr(10, 0);
+    DynamicArray<int> arr(5, 0);
 
-    arr.resize(15);
     arr.resize(10);
-    print_array(arr);
 
     for(int i = 0; i < arr.capacity(); i++)
         arr[i] = i;
-    print_array(arr);
 
-    arr.push_back(15);
     arr.push_back(11);
-    print_array(arr);
-
-    arr.push_back(16);
     arr.push_back(12);
-    print_array(arr);
 
     arr.pop_back();
-    print_array(arr);
 
     arr.insert(10, 10);
-    print_array(arr);
     arr.erase(arr.size() - 1);
 
     // 최종 배열 상태 출력
